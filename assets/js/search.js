c2dee8d191d6b8da27fab6e87ecf612ba3adab15
ngtrduongvn <nguyentrungduong209.qb@gmail.com>
2026-09-13 17:56:06 +0700

    v2 Hotfix 1

--- assets/js/search.js
+++ assets/js/search.js
@@ -1,57 +1,23 @@
-// const searchInput = document.getElementById('searchInput');
-
-//         const rows = document.querySelectorAll('#fileTableBody tr');
-
-//         searchInput.addEventListener('input', function() {
-//             const keyword = searchInput.value.toLowerCase().trim();
-
-//             rows.forEach(function(row) {
-
-//                 const fileNameElement = row.querySelector('td a');
-
-//                 if (fileNameElement) {
-//                     const fileName = fileNameElement.textContent.toLowerCase();
-
-
-//                     if (fileName.includes(keyword)) {
-//                         row.style.display = '';
-//                     } else {
-//                         row.style.display = 'none';
-//                     }
-//                 }
-//             });
-//         });
-
 const searchInput = document.getElementById('searchInput');
-const rows = document.querySelectorAll('#fileTableBody tr');
 
-searchInput.addEventListener('input', function() {
-    const keyword = searchInput.value.toLowerCase().trim();
+        const rows = document.querySelectorAll('#fileTableBody tr');
 
-    rows.forEach(function(row) {
-        const fileNameElement = row.querySelector('td a');
+        searchInput.addEventListener('input', function() {
+            const keyword = searchInput.value.toLowerCase().trim();
 
-        if (fileNameElement) {
-            const fileName = fileNameElement.textContent.toLowerCase();
+            rows.forEach(function(row) {
 
-            if (fileName.includes(keyword)) {
-                // Hiện dòng
-                row.classList.remove('is-removed');
-                // Timeout nhỏ để trình duyệt kịp render display trước khi chạy hiệu ứng fade-in
-                setTimeout(() => {
-                    row.classList.remove('is-hidden');
-                }, 10);
-            } else {
-                // Bắt đầu hiệu ứng fade-out
-                row.classList.add('is-hidden');
-                
-                // Sau khi kết thúc animation 200ms thì ẩn display để không tốn diện tích
-                setTimeout(() => {
-                    if (row.classList.contains('is-hidden')) {
-                        row.classList.add('is-removed');
+                const fileNameElement = row.querySelector('td a');
+
+                if (fileNameElement) {
+                    const fileName = fileNameElement.textContent.toLowerCase();
+
+
+                    if (fileName.includes(keyword)) {
+                        row.style.display = '';
+                    } else {
+                        row.style.display = 'none';
                     }
-                }, 200);
-            }
-        }
-    });
-});
+                }
+            });
+        });
